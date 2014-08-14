752fda44c311a055286c03a7569752210819333f
Mikeal Rogers <mikeal.rogers@gmail.com>
2014-08-14 13:41:00 -0400

    First two tests moved to tape.

--- tests/test-agentOptions.js
+++ tests/test-agentOptions.js
@@ -2,22 +2,39 @@ var request = require('../index')
   , http = require('http')
   , server = require('./server')
   , assert = require('assert')
+  , tape = require('tape')
+  , s
+  , port = 8111
   ;
 
-var s = http.createServer(function (req, resp) {
-  resp.statusCode = 200
-  resp.end('')
-}).listen(6767, function () {
-  // requests without agentOptions should use global agent
-  var r = request('http://localhost:6767', function (e, resp, body) {
-    assert.deepEqual(r.agent, http.globalAgent);
-    assert.equal(Object.keys(r.pool).length, 0);
-
-    // requests with agentOptions should apply agentOptions to new agent in pool
-    var r2 = request('http://localhost:6767', { agentOptions: { foo: 'bar' } }, function (e, resp, body) {
-      assert.equal(r2.agent.options.foo, 'bar');
-      assert.equal(Object.keys(r2.pool).length, 1);
-	    s.close()
- 	 });
+tape('setup', function (t) {
+  s = http.createServer(function (req, resp) {
+    resp.statusCode = 200
+    resp.end('')
+  }).listen(port, function () {
+    t.end()
   })
+})
+
+tape('agent w/o options', function (t) {
+  t.plan(2)
+  // requests without agentOptions should use global agent
+  var r = request('http://localhost:'+port, function (e, resp, body) {
+    t.deepEqual(r.agent, http.globalAgent);
+    t.equal(Object.keys(r.pool).length, 0);
+  })
+})
+
+tape('agent w/ options', function (t) {
+  t.plan(2)
+  // requests with agentOptions should apply agentOptions to new agent in pool
+  var r2 = request('http://localhost:'+port, { agentOptions: { foo: 'bar' } }, function (e, resp, body) {
+    t.equal(r2.agent.options.foo, 'bar');
+    t.equal(Object.keys(r2.pool).length, 1);
+  });
+})
+
+tape('cleaup', function (t) {
+  s.close()
+  t.end()
 })
--- tests/test-basic-auth.js
+++ tests/test-basic-auth.js
@@ -1,191 +1,170 @@
 var assert = require('assert')
   , http = require('http')
   , request = require('../index')
+  , tape = require('tape')
+
+  , basicServer
   ;
 
-var numBasicRequests = 0;
+tape('setup', function (t) {
+  basicServer = http.createServer(function (req, res) {
+    console.log('Basic auth server: ', req.method, req.url)
 
-var basicServer = http.createServer(function (req, res) {
-  console.error('Basic auth server: ', req.method, req.url);
-  numBasicRequests++;
+    var ok
 
-  var ok;
-
-  if (req.headers.authorization) {
-    if (req.headers.authorization == 'Basic ' + new Buffer('test:testing2').toString('base64')) {
-      ok = true;
-    } else if ( req.headers.authorization == 'Basic ' + new Buffer('test:').toString('base64')) {
-      ok = true;
-    } else if ( req.headers.authorization == 'Basic ' + new Buffer(':apassword').toString('base64')) {
-      ok = true;
-    } else if ( req.headers.authorization == 'Basic ' + new Buffer('justauser').toString('base64')) {
-      ok = true;
+    if (req.headers.authorization) {
+      if (req.headers.authorization == 'Basic ' + new Buffer('test:testing2').toString('base64')) {
+        ok = true
+      } else if ( req.headers.authorization == 'Basic ' + new Buffer('test:').toString('base64')) {
+        ok = true
+      } else if ( req.headers.authorization == 'Basic ' + new Buffer(':apassword').toString('base64')) {
+        ok = true
+      } else if ( req.headers.authorization == 'Basic ' + new Buffer('justauser').toString('base64')) {
+        ok = true
+      } else {
+        // Bad auth header, don't send back WWW-Authenticate header
+        ok = false
+      }
     } else {
-      // Bad auth header, don't send back WWW-Authenticate header
-      ok = false;
+      // No auth header, send back WWW-Authenticate header
+      ok = false
+      res.setHeader('www-authenticate', 'Basic realm="Private"')
     }
-  } else {
-    // No auth header, send back WWW-Authenticate header
-    ok = false;
-    res.setHeader('www-authenticate', 'Basic realm="Private"');
-  }
 
-  if (req.url == '/post/') {
-    var expectedContent = 'data_key=data_value';
-    req.on('data', function(data) {
-      assert.equal(data, expectedContent);
-      console.log('received request data: ' + data);
-    });
-    assert.equal(req.method, 'POST');
-    assert.equal(req.headers['content-length'], '' + expectedContent.length);
-    assert.equal(req.headers['content-type'], 'application/x-www-form-urlencoded; charset=utf-8');
-  }
-
-  if (ok) {
-    console.log('request ok');
-    res.end('ok');
-  } else {
-    console.log('status=401');
-    res.statusCode = 401;
-    res.end('401');
-  }
-});
-
-basicServer.listen(6767);
-
-var tests = [
-  function(next) {
-    request({
-      'method': 'GET',
-      'uri': 'http://localhost:6767/test/',
-      'auth': {
-        'user': 'test',
-        'pass': 'testing2',
-        'sendImmediately': false
-      }
-    }, function(error, res, body) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(numBasicRequests, 2);
-      next();
-    });
-  },
-
-  function(next) {
-    // If we don't set sendImmediately = false, request will send basic auth
-    request({
-      'method': 'GET',
-      'uri': 'http://localhost:6767/test2/',
-      'auth': {
-        'user': 'test',
-        'pass': 'testing2'
-      }
-    }, function(error, res, body) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(numBasicRequests, 3);
-      next();
-    });
-  },
-
-  function(next) {
-    request({
-      'method': 'GET',
-      'uri': 'http://test:testing2@localhost:6767/test2/'
-    }, function(error, res, body) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(numBasicRequests, 4);
-      next();
-    });
-  },
-
-  function(next) {
-    request({
-      'method': 'POST',
-      'form': { 'data_key': 'data_value' },
-      'uri': 'http://localhost:6767/post/',
-      'auth': {
-        'user': 'test',
-        'pass': 'testing2',
-        'sendImmediately': false
-      }
-    }, function(error, res, body) {
-      assert.equal(res.statusCode, 200);
-      assert.equal(numBasicRequests, 6);
-      next();
-    });
-  },
-
-  function(next) {
-    assert.doesNotThrow( function() {
-      request({
-        'method': 'GET',
-        'uri': 'http://localhost:6767/allow_empty_user/',
-        'auth': {
-          'user': '',
-          'pass': 'apassword',
-          'sendImmediately': false
-        }
-      }, function(error, res, body ) {
-        assert.equal(res.statusCode, 200);
-        assert.equal(numBasicRequests, 8);
-        next();
+    if (req.url == '/post/') {
+      var expectedContent = 'data_key=data_value'
+      req.on('data', function(data) {
+        basicServer.t.equal(data.toString(), expectedContent)
+        console.log('received request data: ' + data)
       });
-    })
-  },
+      basicServer.t.equal(req.method, 'POST')
+      basicServer.t.equal(req.headers['content-length'], '' + expectedContent.length)
+      basicServer.t.equal(req.headers['content-type'], 'application/x-www-form-urlencoded; charset=utf-8');
+    }
 
-  function(next) {
-    assert.doesNotThrow( function() {
-      request({
-        'method': 'GET',
-        'uri': 'http://localhost:6767/allow_undefined_password/',
-        'auth': {
-          'user': 'justauser',
-          'pass': undefined,
-          'sendImmediately': false
-        }
-      }, function(error, res, body ) {
-        assert.equal(res.statusCode, 200);
-        assert.equal(numBasicRequests, 10);
-        next();
-      });
-    })
-  },
+    if (ok) {
+      res.end('ok')
+    } else {
+      res.statusCode = 401
+      res.end('401')
+    }
+  })
+  basicServer.listen(6767, t.end.bind(t))
+})
 
-  function (next) {
-    request
-      .get('http://localhost:6767/test/')
-      .auth("test","",false)
-      .on('response', function (res) {
-        assert.equal(res.statusCode, 200);
-        assert.equal(numBasicRequests, 12);
-        next();
-      })
-  },
+tape('sendImmediately false', function (t) {
+  t.plan(1)
+  request({
+    'method': 'GET',
+    'uri': 'http://localhost:6767/test/',
+    'auth': {
+      'user': 'test',
+      'pass': 'testing2',
+      'sendImmediately': false
+    }
+  }, function(error, res, body) {
+    t.equal(res.statusCode, 200)
+  })
+})
 
-  function (next) {
-    request.get('http://localhost:6767/test/',
-      {
-        auth: {
-          user: "test",
-          pass: "",
-          sendImmediately: false
-        }
-      }, function (err, res) {
-        assert.equal(res.statusCode, 200);
-        assert.equal(numBasicRequests, 14);
-        next();
-      })
-  }
-];
+tape('sendImmediately default', function (t) {
+  t.plan(1)
+  // If we don't set sendImmediately = false, request will send basic auth
+  request({
+    'method': 'GET',
+    'uri': 'http://localhost:6767/test2/',
+    'auth': {
+      'user': 'test',
+      'pass': 'testing2'
+    }
+  }, function(error, res, body) {
+    t.equal(res.statusCode, 200)
+  })
+})
 
-function runTest(i) {
-  if (i < tests.length) {
-    tests[i](function() {
-      runTest(i + 1);
-    });
-  } else {
-    console.log('All tests passed');
-    basicServer.close();
-  }
-}
+tape('auth in url', function (t) {
+  t.plan(1)
+  request({
+    'method': 'GET',
+    'uri': 'http://test:testing2@localhost:6767/test2/'
+  }, function(error, res, body) {
+    t.equal(res.statusCode, 200)
+  })
+})
 
-runTest(0);
+tape('form post w/ sendImmediately false', function (t) {
+  basicServer.t = t
+  request({
+    'method': 'POST',
+    'form': { 'data_key': 'data_value' },
+    'uri': 'http://localhost:6767/post/',
+    'auth': {
+      'user': 'test',
+      'pass': 'testing2',
+      'sendImmediately': false
+    }
+  }, function(error, res, body) {
+    t.equal(res.statusCode, 200)
+    basicServer.t = null
+    t.end()
+  })
+})
+
+tape('allow empty user', function (t) {
+  t.plan(1)
+  request({
+    'method': 'GET',
+    'uri': 'http://localhost:6767/allow_empty_user/',
+    'auth': {
+      'user': '',
+      'pass': 'apassword',
+      'sendImmediately': false
+    }
+  }, function(error, res, body ) {
+    t.equal(res.statusCode, 200)
+  })
+})
+
+tape('allow undefined password', function (t) {
+  t.plan(1)
+  request({
+    'method': 'GET',
+    'uri': 'http://localhost:6767/allow_undefined_password/',
+    'auth': {
+      'user': 'justauser',
+      'pass': undefined,
+      'sendImmediately': false
+    }
+  }, function(error, res, body ) {
+    t.equal(res.statusCode, 200)
+  })
+})
+
+tape('chain api', function (t) {
+  t.plan(1)
+  request
+  .get('http://localhost:6767/test/')
+  .auth("test","",false)
+  .on('response', function (res) {
+    t.equal(res.statusCode, 200);
+  })
+})
+
+tape('empty pass w/ sendImmediately false', function (t) {
+  t.plan(1)
+  request.get('http://localhost:6767/test/',
+  {
+    auth: {
+      user: "test",
+      pass: "",
+      sendImmediately: false
+    }
+  }, function (err, res) {
+    t.equal(res.statusCode, 200)
+  })
+})
+
+tape('teardown', function (t) {
+  basicServer.close()
+  t.end()
+})
